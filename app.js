1fd5a1682d9166ea4fd726dabd2e3f9cc6977bf0
Jorge Armando Pérez Garay <joarpega@gmail.com>
2018-10-14 03:25:56 -0500

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
 // Requires : importacion de librerias.
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 // Inicializacion de variables.
 var app = express();
+
+// BodyParser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+    // parse application/json
+app.use(bodyParser.json())
+
+// Importar RUTAS
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 // Conexion a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
     if (err) throw err;
     console.log('Base de datos: \x1b[32m%s\x1b[0m', ' online');
-})
+});
 
 // Rutas
-app.get('/', (req, res, next) => {
-
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    });
-
-});
+//se declara un midelware (es algo que se ejecuta antes que se resuelvan otras rutas)
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 // Escuchar peticiones.
 app.listen(3000, () => {
